4bf7188f1ed08d83e2430fc5d4ad35324d16c22a
Daniele <danieletortoli@gmail.com>
2023-05-23 16:39:14 +0200

    Add new Project

--- app/src/main/java/com/example/beproducktive/ui/projects/ProjectsFragment.kt
+++ app/src/main/java/com/example/beproducktive/ui/projects/ProjectsFragment.kt
@@ -6,13 +6,20 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beproducktive.R
 import com.example.beproducktive.databinding.FragmentProjectsBinding
+import com.example.beproducktive.utils.exhaustive
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class ProjectsFragment : Fragment(R.layout.fragment_projects) {
@@ -24,10 +31,14 @@ class ProjectsFragment : Fragment(R.layout.fragment_projects) {
 
         val binding = FragmentProjectsBinding.bind(view)
 
-        val projectAdapter = ProjectsAdapter(ProjectsAdapter.OnClickListener {project ->
+        val projectAdapter = ProjectsAdapter(ProjectsAdapter.OnClickListener { project ->
 //            Log.d("ONCLICK", "CLICKED PROJECT")
             Toast.makeText(requireContext(), project.projectName, Toast.LENGTH_SHORT).show()
-            findNavController().navigate(ProjectsFragmentDirections.actionProjectsFragmentToTasksFragment(project))
+            findNavController().navigate(
+                ProjectsFragmentDirections.actionProjectsFragmentToTasksFragment(
+                    project
+                )
+            )
 
         })
 
@@ -39,16 +50,67 @@ class ProjectsFragment : Fragment(R.layout.fragment_projects) {
                 setHasFixedSize(true)
             }
 
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+
+                override fun onMove(
+                    recyclerView: androidx.recyclerview.widget.RecyclerView,
+                    viewHolder: androidx.recyclerview.widget.RecyclerView.ViewHolder,
+                    target: androidx.recyclerview.widget.RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(
+                    viewHolder: androidx.recyclerview.widget.RecyclerView.ViewHolder,
+                    direction: Int
+                ) {
+                    val project = projectAdapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onProjectSwiped(project)
+                }
+
+            }).attachToRecyclerView(recyclerViewProjects)
+
+
+            editNewProject.addTextChangedListener {
+                viewModel.projName = it.toString()
+            }
+
             buttonNewProject.setOnClickListener {
-//                findNavController().navigate(R.id.action_projectsFragment_to_addProjectFragment)
-//                val AddProjectFragment = AddProjectFragment()
-//                AddProjectFragment.show(requireActivity().supportFragmentManager, "AddProjectFragment")
+                viewModel.onClickNewProject(viewModel.projName)
             }
 
         }
 
         viewModel.projects.observe(viewLifecycleOwner) { projectsList ->
             projectAdapter.submitList(projectsList)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.addProjectEvent.collect { event ->
+                when (event) {
+                    is ProjectsViewModel.ProjectsEvent.AddProject -> {
+                        binding.editNewProject.clearFocus()
+                        // navigate to the same fragment with navcontroller to refresh the list
+                        val action = ProjectsFragmentDirections.actionProjectsFragmentSelf()
+                        findNavController().navigate(action)
+                        Snackbar.make(requireView(), "Project added", Snackbar.LENGTH_SHORT).show()
+                    }
+                    is ProjectsViewModel.ProjectsEvent.ShowInvalidInputMessage -> {
+                        binding.editNewProject.clearFocus()
+                        Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_SHORT).show()
+                    }
+                    is ProjectsViewModel.ProjectsEvent.ShowUndoDeleteProjectMessage -> {
+                        Snackbar.make(requireView(), "Project deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO") {
+                                viewModel.onUndoDeleteClick(event.project)
+                            }.show()
+                    }
+                }.exhaustive
+            }
+
         }
 
     }
--- app/src/main/java/com/example/beproducktive/ui/projects/ProjectsViewModel.kt
+++ app/src/main/java/com/example/beproducktive/ui/projects/ProjectsViewModel.kt
@@ -8,8 +8,11 @@ import com.example.beproducktive.data.projects.ProjectRepository
 import com.example.beproducktive.data.tasks.Task
 import com.example.beproducktive.data.tasks.TaskDao
 import com.example.beproducktive.data.tasks.TaskPriority
+import com.example.beproducktive.ui.addedittasks.AddEditViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
@@ -20,7 +23,46 @@ class ProjectsViewModel @Inject constructor(
     private val projectRepository: ProjectRepository
 ) : ViewModel() {
 
+    var projName: String = ""
     val projects = projectRepository.getProjects().asLiveData()
 
+    private val addProjectEventChannel = Channel<ProjectsEvent>()
+    val addProjectEvent = addProjectEventChannel.receiveAsFlow()
+
+
+    fun onClickNewProject(projectName: String) {
+        if (projectName.isBlank()) {
+            showInvalidInputMessage("Project name cannot be empty.")
+            return
+        }
+        addProject(projectName)
+    }
+
+    private fun addProject(projectName: String) = viewModelScope.launch {
+        val project = Project(projectName)
+        projectRepository.insert(project)
+        addProjectEventChannel.send(ProjectsEvent.AddProject(project))
+    }
+
+    private fun showInvalidInputMessage(text: String) = viewModelScope.launch {
+        addProjectEventChannel.send(ProjectsEvent.ShowInvalidInputMessage(text))
+    }
+
+    fun onProjectSwiped(project: Project) = viewModelScope.launch {
+        projectRepository.delete(project)
+        addProjectEventChannel.send(ProjectsEvent.ShowUndoDeleteProjectMessage(project))
+    }
+
+    fun onUndoDeleteClick(project: Project) = viewModelScope.launch {
+        projectRepository.insert(project)
+    }
+
+
+    sealed class ProjectsEvent {
+        data class AddProject(val project: Project) : ProjectsEvent()
+        data class ShowInvalidInputMessage(val msg: String) : ProjectsEvent()
+
+        data class ShowUndoDeleteProjectMessage(val project: Project) : ProjectsEvent()
+    }
 
 }
